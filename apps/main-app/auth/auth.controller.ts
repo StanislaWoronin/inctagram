6c9fe40b6c62bb0483938385ba38288899d0cb61
Stanislav <stanislaworonin@yandex.ru>
2023-07-26 14:09:08 +0300

    ADD: create logging middleware

--- apps/main-app/auth/auth.controller.ts
+++ apps/main-app/auth/auth.controller.ts
@@ -141,7 +141,6 @@ export class AuthController {
     return await this.userFacade.commands.updatePairToken(dto);
   }
 
-
   // Registration in the system
   @Post(authEndpoints.registration())
   @ApiRegistration()
